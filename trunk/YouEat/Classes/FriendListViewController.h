e4fcb287e27126a32e12e65dd9cb334cab062b01
a.vincelli <a.vincelli@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2010-07-23 19:17:34 +0000

    added list of friends Fixes Issue 5

---
+++ trunk/YouEat/Classes/FriendListViewController.h
@@ -0,0 +1,27 @@
+//
+//  FriendListViewController.h
+//  YouEat
+//
+//  Created by Alessandro Vincelli on 7/23/10.
+//  Copyright 2010 ICTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RestUtil.h"
+	
+@class ASIHTTPRequest;
+
+@interface FriendListViewController : UITableViewController {
+	
+	NSMutableArray *listOfUser;
+	IBOutlet UITableView *tableViewUser;
+	RestUtil *restUtil;
+	ASIHTTPRequest *request;
+}
+
+@property (nonatomic, retain) IBOutlet UITableView *tableViewUser;
+@property (nonatomic, retain) IBOutlet NSMutableArray *listOfUser;
+@property (nonatomic, retain) RestUtil *restUtil;
+@property (retain, nonatomic) ASIHTTPRequest *request;
+
+@end

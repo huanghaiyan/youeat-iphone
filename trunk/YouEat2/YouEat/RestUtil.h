eca5aacd8810045f30804adba9a6a50beb6f12f6
a.vincelli@gmail.com <a.vincelli@gmail.com@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2011-10-05 17:06:11 +0000

    rest call now are sync and JSON abject are parsed using stream

--- trunk/YouEat2/YouEat/RestUtil.h
+++ trunk/YouEat2/YouEat/RestUtil.h
@@ -7,15 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JSON/SBJSON.h"
+
+@protocol YRestUtilDelegate
+
+/**
+ @brief Called when a JSON object is received and parsed
+ 
+ This method is called when a JSON object is received and parsed
+ */
+- (void)responseParsed: (NSArray*)array;
+
+@end
 
 
-@interface RestUtil : NSObject {
+@interface RestUtil : NSObject <SBJsonStreamParserAdapterDelegate>{
 	
 	NSString *connectionURL;
+
+@private
+    SBJsonParser *_parser;
+    SBJsonWriter *_writer;
+    SBJsonStreamParser *_streamParser;
+    SBJsonStreamParserAdapter *_adapter;
+    id<YRestUtilDelegate> delegate;
 }
 
 @property (nonatomic, retain) IBOutlet NSString *connectionURL;
+@property (nonatomic, assign) id<YRestUtilDelegate> delegate;
 
-- (NSDictionary*) sendRestRequest:(NSString*)url;
+- (void) sendRestRequest:(NSString*)url;
 
 @end
--- trunk/YouEat2/YouEat/RestUtil.m
+++ trunk/YouEat2/YouEat/RestUtil.m
@@ -7,34 +7,51 @@
 //
 
 #import "RestUtil.h"
-#import "JSON/JSON.h"
+
 
 @implementation RestUtil
 
-@synthesize connectionURL;
+@synthesize connectionURL, delegate;
 
-- (NSDictionary*) sendRestRequest:(NSString *)url{	
-	NSURLRequest *request;
-	NSDictionary *statuses;	
-	
-	SBJSON *parser = [[SBJSON alloc] init];
-	
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _parser = [[SBJsonParser alloc] init];
+        _writer = [[SBJsonWriter alloc] init];
+        _writer.humanReadable = YES;
+        _writer.sortKeys = YES;
+        // We don't want *all* the individual messages from the
+        // SBJsonStreamParser, just the top-level objects. The stream
+        // parser adapter exists for this purpose.
+        _adapter = [[SBJsonStreamParserAdapter alloc] init];
+        
+        // Set ourselves as the delegate, so we receive the messages
+        // from the adapter.
+        _adapter.delegate = self;
+        
+        // Create a new stream parser..
+        _streamParser = [[SBJsonStreamParser alloc] init];
+        
+        // .. and set our adapter as its delegate.
+        _streamParser.delegate = _adapter;
+        
+        // Normally it's an error if JSON is followed by anything but
+        // whitespace. Setting this means that the parser will be
+        // expecting the stream to contain multiple whitespace-separated
+        // JSON documents.
+        _streamParser.supportMultipleDocuments = YES;
+
+    }
+    return self;
+}
+
+- (void) sendRestRequest:(NSString *)url{	
 	NSString *urlString = [[self connectionURL] stringByAppendingString:url]; 
-	request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];	
-	
-	//	[[NSURLConnection alloc] initWithRequest:request delegate:self];
-	
-	// Perform request and get JSON back as a NSData object
-	NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-	
-	// Get JSON as a NSString from NSData response
-	NSString *json_string = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
-	
-	// parse the JSON response into an object
-	// Here we're using NSArray since we're parsing an array of JSON status objects
-	statuses = [parser objectWithString:json_string error:nil];
-	[parser release];
-	return statuses;
+    NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]
+											  cachePolicy:NSURLRequestUseProtocolCachePolicy
+										  timeoutInterval:60.0];	
+	[[[NSURLConnection alloc] initWithRequest:theRequest delegate:self] autorelease];
 }
 
 - (NSString *)connectionURL {
@@ -50,8 +67,65 @@
 	return connectionURL;
 }
 
-- (void)dealloc {
-	[connectionURL release];
+#pragma mark SBJsonStreamParserAdapterDelegate methods
+
+- (void)parser:(SBJsonStreamParser *)parser foundArray:(NSArray *)array {
+    [NSException raise:@"unexpected" format:@"Should not get here"];
+}
+
+- (void)parser:(SBJsonStreamParser *)parser foundObject:(NSDictionary *)dict {
+    // contains the list of NSDictionary(Risto Details)
+    //TODO send the temposs to calling paged
+    NSArray *temposs = [dict objectForKey:@"ristoranteList"];
+    [delegate responseParsed:temposs];
+    NSLog(@"Connection didReceiveResponse: %@ ", [dict objectForKey:@"ristoranteList"]);
+}
+
+
+#pragma mark NSURLConnectionDelegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	NSLog(@"Connection didReceiveResponse: %@ - %@", response, [response MIMEType]);
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+	NSLog(@"Connection didReceiveAuthenticationChallenge: %@", challenge);
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	NSLog(@"Connection didReceiveData of length: %u", data.length);
+	
+	// Parse the new chunk of data. The parser will append it to
+	// its internal buffer, then parse from where it left off in
+	// the last chunk.
+    SBJsonStreamParserStatus status = [_streamParser parse:data];
+    
+    if (status == SBJsonStreamParserError) {
+        //tweet.text = [NSString stringWithFormat: @"The parser encountered an error: %@", _streamParser.error];
+        NSLog(@"Parser error: %@", _streamParser.error);
+        
+    } else if (status == SBJsonStreamParserWaitingForData) {
+        NSLog(@"Parser waiting for more data");
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"Connection failed! Error - %@ %@",
+          [error localizedDescription],
+          [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    	NSLog(@"Connection connectionDidFinishLoading");
+}
+
+- (void)dealloc
+{
+    [_parser release];
+    [_writer release];
+    [_streamParser release];
+    [_adapter release];
+  	[connectionURL release];
     [super dealloc];
 }
 

4ab95e85ba253c3eaaf47fba7659be8e3d5257b2
a.vincelli@gmail.com <a.vincelli@gmail.com@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2011-10-07 16:18:15 +0000

    working on restaurants list

--- trunk/YouEat2/YouEat/ListRestaurants.m
+++ trunk/YouEat2/YouEat/ListRestaurants.m
@@ -38,7 +38,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.navigationItem.title = @"your title";
+    self.navigationItem.title = @"Results";
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
@@ -99,24 +99,113 @@
 	NSDictionary *ristoItem = [[ristos objectAtIndex:indexPath.row] objectForKey:@"ristorante"];    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell =
-        [[[UITableViewCell alloc]
-          initWithFrame:CGRectZero
-          reuseIdentifier:CellIdentifier]
-         autorelease];
+        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
     }
-    UILabel *topLabel =
-    [[[UILabel alloc]
-      initWithFrame:
-      CGRectMake(
-                 cell.indentationWidth,
-                 5.0f,
-                 cell.frame.size.width,
-                 20.0f)]
-     autorelease];
+    // TOP LABEL - TITLE
+    UILabel *topLabel = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth, 2.0f, cell.frame.size.width, 18.0f)] autorelease];
+    topLabel.textColor = [UIColor colorWithRed:0.25 green:0.0 blue:0.0 alpha:1.0];
+    [topLabel setFont:[UIFont fontWithName:@"Verdana-Bold" size:[UIFont smallSystemFontSize]]];
+    [topLabel setTextColor: [UIColor colorWithRed:0.9 green:0.4 blue:0.0 alpha:1]];
+    topLabel.adjustsFontSizeToFitWidth = YES;	
     topLabel.text = [ristoItem objectForKey:@"name"];
     [cell.contentView addSubview:topLabel];
+    
+    // ADDRESS and DISTANCE LABEL
+    UILabel *addrLabel = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth, 20.0f, cell.frame.size.width, 10.0f)] autorelease];
+    [addrLabel setFont:[UIFont fontWithName:@"Verdana" size:[UIFont smallSystemFontSize] - 4 ]];
+    addrLabel.adjustsFontSizeToFitWidth = YES;
+    NSString *city = [[ristoItem objectForKey:@"city"] objectForKey:@"name"];
+    NSString *address = [ristoItem objectForKey:@"address"];
+    NSDecimalNumber *distanceInMeters = [[ristos objectAtIndex:indexPath.row] objectForKey:@"distanceInMeters"];
+    int intDistance = distanceInMeters.intValue > 1000 ? distanceInMeters.intValue / 100 : distanceInMeters.intValue;
+    NSString *distance = [NSString stringWithFormat: @"%u%@", intDistance, (distanceInMeters.intValue > 1000) ? @"km" : @"m"];
+    addrLabel.text = [NSString stringWithFormat:@"%@, %@ - %@", city, address, distance]; 
+    [cell.contentView addSubview:addrLabel];
 
+    
+    // IMG LABEL
+    UILabel *imgLabel = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth, 35.0f, 80.0f, 80.0f)] autorelease];
+    [imgLabel setBackgroundColor:[UIColor lightGrayColor]];
+    [cell.contentView addSubview:imgLabel];
+
+    // RATING STARS LABEL
+    NSNumber *rating = [ristoItem objectForKey:@"rating"];
+    UIImage *star = [UIImage imageNamed:@"star-gold-mini.png"];
+    UIImage *starOff = [UIImage imageNamed:@"star-gold-mini-off.png"];
+    
+    UILabel *starLabel = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth + 90.0f, 35.0f, 10.0f, 10.0f)] autorelease];
+    UIColor *starLabelImg = [[[UIColor alloc] initWithPatternImage: (rating.intValue) < 1 ? starOff : star ] autorelease];
+    [starLabel setBackgroundColor:starLabelImg];
+    [cell.contentView addSubview:starLabel];
+    
+    UILabel *starLabel2 = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth + 102.0f, 35.0f, 10.0f, 10.0f)] autorelease];
+    UIColor *starLabelImg2 = [[[UIColor alloc] initWithPatternImage: rating.intValue < 2 ? starOff : star ] autorelease];
+    [starLabel2 setBackgroundColor:starLabelImg2];
+    [cell.contentView addSubview:starLabel2];
+    
+    UILabel *starLabel3 = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth + 114.0f, 35.0f, 10.0f, 10.0f)] autorelease];
+    UIColor *starLabelImg3 = [[[UIColor alloc] initWithPatternImage: rating.intValue < 3 ? starOff : star ] autorelease];
+    [starLabel3 setBackgroundColor:starLabelImg3];
+    [cell.contentView addSubview:starLabel3];
+    
+    UILabel *starLabel4 = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth + 126.0f, 35.0f, 10.0f, 10.0f)] autorelease];
+    UIColor *starLabelImg4 = [[[UIColor alloc] initWithPatternImage: rating.intValue < 4 ? starOff : star ] autorelease];
+    [starLabel4 setBackgroundColor:starLabelImg4];
+    [cell.contentView addSubview:starLabel4];
+    
+    UILabel *starLabel5 = [[[UILabel alloc] initWithFrame: CGRectMake(cell.indentationWidth + 138.0f, 35.0f, 10.0f, 10.0f)] autorelease];
+    UIColor *starLabelImg5 = [[[UIColor alloc] initWithPatternImage: rating.intValue < 5 ? starOff : star ] autorelease];
+    [starLabel5 setBackgroundColor:starLabelImg5];
+    [cell.contentView addSubview:starLabel5];
+
+    // PHONE LABEL
+	NSString *phoneNumber = [ristoItem objectForKey:@"phoneNumber"];
+	NSString *mobilePhoneNumber = [ristoItem objectForKey:@"mobilePhoneNumber"];
+	
+	if(phoneNumber != nil && phoneNumber != NULL && (NSNull *)phoneNumber != [NSNull null]){
+		phoneNumber = [phoneNumber stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+		phoneNumber = [phoneNumber stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"-"]];
+	}
+    else if(mobilePhoneNumber != nil && mobilePhoneNumber != NULL && (NSNull *)mobilePhoneNumber != [NSNull null]){
+		mobilePhoneNumber = [mobilePhoneNumber stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+		mobilePhoneNumber = [mobilePhoneNumber stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"-"]];
+	}
+    UIButton *callButton = [UIButton buttonWithType: UIButtonTypeCustom];
+    [callButton setFrame: CGRectMake(cell.indentationWidth + 210.0f, 35.0f, 100.0f, 15.0f)];
+    [callButton.titleLabel setFont:[UIFont fontWithName:@"Verdana-Bold" size:[UIFont smallSystemFontSize] -4]];
+    [callButton setTitle:phoneNumber forState:UIControlStateNormal];
+    [callButton setTitleColor:[UIColor colorWithRed:0.9 green:0.4 blue:0.0 alpha:1] forState:UIControlStateNormal];
+    [callButton sizeToFit];
+    [cell.contentView addSubview:callButton];
+    
+    //TODO attach the call to button above
+//    UIDevice *device = [UIDevice currentDevice];
+//    if ([[device model] isEqualToString:@"iPhone"] ) {
+//        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel:130-032-2837"]]];
+//    } else {
+//        UIAlertView *Notpermitted=[[UIAlertView alloc] initWithTitle:@"Alert" message:@"Your device doesn't support this feature." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+//        [Notpermitted show];
+//        [Notpermitted release];
+//    }
+
+    // DESCRIPTION LABEL
+    NSString * language = [[NSLocale preferredLanguages] objectAtIndex:0];
+    UITextView *descriptionView = [[[UITextView alloc] initWithFrame: CGRectMake(cell.indentationWidth + 85.0f, 55.0f, 220.0f, 70.0f)] autorelease];
+	NSDictionary *descriptions = [ristoItem objectForKey:@"descriptions"] ;		
+	NSString *descriptionText = @"";	
+    for (NSDictionary *descriptionItem in descriptions) {
+		NSString *descItem = [descriptionItem objectForKey:@"description"];
+   		NSString *langItem = [[descriptionItem objectForKey:@"language"] objectForKey:@"language"];
+		if([langItem isEqualToString: language] && descItem != NULL && (NSNull *)descItem != [NSNull null] && descItem.length > 0){
+			descriptionText = [descriptionText stringByAppendingString:descItem];		
+		}
+
+    }
+    [descriptionView setEditable: FALSE];
+    [descriptionView setFont:[UIFont fontWithName:@"Verdana" size:[UIFont smallSystemFontSize] - 4 ]];
+    descriptionView.text = descriptionText;
+    [cell.contentView addSubview:descriptionView];
+    
 //    if (cell == nil) {
 //        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
 //		cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;

35be70fc9cab27698f2a95156688771c073e7b5b
a.vincelli <a.vincelli@gmail.com>
2011-10-12 08:32:18 +0000

    added About Page

---
+++ YouEat2/YouEat/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  YouEat
+//
+//  Created by Alessandro Vincelli on 10/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController <UIWebViewDelegate>
+
+@property (nonatomic, retain) UIWebView *myWebView;
+
+@end

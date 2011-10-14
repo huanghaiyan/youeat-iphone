b4a11d7b0feedae66f3bcac37f1979c6c3cb113a
a.vincelli@gmail.com <a.vincelli@gmail.com@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2011-10-14 14:47:26 +0000

    new ios5 version starts

---
+++ trunk/ios5/youeat/youeat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  youeat
+//
+//  Created by Alessandro Vincelli on 10/14/11.
+//  Copyright (c) 2011 Wedjaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AVAppDelegate class]));
+    }
+}

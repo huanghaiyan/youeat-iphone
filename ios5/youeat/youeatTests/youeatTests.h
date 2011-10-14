91f1f9f145d4c971b9cc2e1fdcd5a3fcb9d3a032
a.vincelli <a.vincelli@gmail.com>
2011-10-14 14:47:26 +0000

    new ios5 version starts

---
+++ ios5/youeat/youeatTests/youeatTests.h
@@ -0,0 +1,13 @@
+//
+//  youeatTests.h
+//  youeatTests
+//
+//  Created by Alessandro Vincelli on 10/14/11.
+//  Copyright (c) 2011 Wedjaa. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface youeatTests : SenTestCase
+
+@end
---
+++ ios5/youeat/youeatTests/youeatTests.m
@@ -0,0 +1,32 @@
+//
+//  youeatTests.m
+//  youeatTests
+//
+//  Created by Alessandro Vincelli on 10/14/11.
+//  Copyright (c) 2011 Wedjaa. All rights reserved.
+//
+
+#import "youeatTests.h"
+
+@implementation youeatTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in youeatTests");
+}
+
+@end

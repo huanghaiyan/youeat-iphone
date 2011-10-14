bbdf23113d452641de939e8af52b3bc9c04f64cf
a.vincelli@gmail.com <a.vincelli@gmail.com@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2011-10-14 14:46:20 +0000

    new ios5 version starts

---
+++ trunk/ios5/youeat/AVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AVAppDelegate.h
+//  youeat
+//
+//  Created by Alessandro Vincelli on 10/14/11.
+//  Copyright (c) 2011 Wedjaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

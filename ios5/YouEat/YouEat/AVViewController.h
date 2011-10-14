783e9e203fceeed635863c8e6717f28cf03cd49b
a.vincelli <a.vincelli@gmail.com>
2011-10-14 14:52:53 +0000

    new ios5 version starts

---
+++ ios5/YouEat/YouEat/AVViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVViewController.h
+//  YouEat
+//
+//  Created by Alessandro Vincelli on 10/14/11.
+//  Copyright (c) 2011 Wedjaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVViewController : UIViewController
+
+@end

4ccd94e43a42a6e3e4f172735967a99d2092aec3
a.vincelli@gmail.com <a.vincelli@gmail.com@6b565749-c4bb-7e99-159d-c5297a1c3b1e>
2011-10-14 19:59:34 +0000

    committing...

--- trunk/ios5/YouEat/YouEat/AVViewController.m
+++ trunk/ios5/YouEat/YouEat/AVViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "AVViewController.h"
+#import "ListRestaurants.h"
+#import "AboutViewController.h"
 
 @implementation AVViewController
+
+@synthesize restUtil, listOfRisto, alertView, searchInput, locationManager, location, aboutBtn;
 
 - (void)didReceiveMemoryWarning
 {
@@ -18,17 +22,151 @@
 
 #pragma mark - View lifecycle
 
+- (void) searchRisto:(NSString *)searchText{
+	
+	NSString *urlString = @"";
+	NSString *pattern = [searchText stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+    
+	if([searchText length] > 2 && location == nil) {
+        //findPaginatedRistoranti/{pattern}/{firstResult}/{maxResults}
+        urlString = [NSString stringWithFormat:@"/findFreeTextSearchCloseRistoranti/%@/%@/%@/%@/%@/%@", pattern, @"1", @"1", @"90000", @"0", @"1"];    
+	}
+    else if([searchText length] > 2 && location != nil) {
+        NSString *latitude = [NSString stringWithFormat:@"%f",location.coordinate.latitude];
+        NSString *longitude = [NSString stringWithFormat:@"%f",location.coordinate.longitude];
+        //findFreeTextSearchCloseRistoranti/{pattern}/{latitude}/{longitude}/{distanceInMeters}/{firstResult}/{maxResults}
+        urlString = [NSString stringWithFormat:@"/findFreeTextSearchCloseRistoranti/%@/%@/%@/%@/%@/%@", pattern, latitude, longitude, @"90000", @"0", @"1"]; 
+        
+    }
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    // TODO performs the search
+    [restUtil sendRestRequest:urlString];
+    
+}
+
+- (void) searchBarSearchButtonClicked:(UIButton *)uiButton {
+    UIImage *backgroundImage = [UIImage imageNamed:@"bg-alert.png"];
+    alertView = [[JKCustomAlert alloc] initWithImage:backgroundImage text:NSLocalizedString(@"Ricerca ristoranti in corso", nil) delegate: self];
+    [alertView show];
+  	[self searchRisto:@"pizzeria"];
+    //	[self searchRisto:searchInput.text];
+}
+
+- (void) goToAbout:(UIButton *)uiButton {
+//  AboutViewController *aboutPage = [[AboutViewController alloc] init];
+    [self performSegueWithIdentifier:@"home2about" sender:self];
+//    [[self navigationController] pushViewController: aboutPage animated: YES];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    restUtil = [[RestUtil alloc] init];
+    [restUtil setDelegate:self];
+    self.navigationItem.title = @"YouEat";
+    
+    UIColor *background = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"bg.png"]];
+    [self.view setBackgroundColor:background];
+    
+    // SEARCH button
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [searchBtn setFrame:CGRectMake(80.0f, 220.0f, 160.0f, 35.0f)];
+    [searchBtn setTitle:@"Cerca" forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(searchBarSearchButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:searchBtn];
+    
+    
+    // SEARCH title label
+    UILabel *searchTitle = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 100.0f, 15.0f)];
+    [searchTitle setText:@"Search risto"];
+    [searchTitle setBackgroundColor:[UIColor colorWithWhite:1 alpha:0]];
+    [searchTitle setTextColor:[UIColor grayColor]];
+    
+    
+    // SEARCH INPUT
+    [self setSearchInput: [[UITextField alloc] init]];
+    [searchInput setFrame:CGRectMake(10.0f, 40.0f, 260.0f, 30.0f)];
+    [searchInput setPlaceholder:@"Search by name, city, tag"];
+    [searchInput setDelegate: self];
+    [searchInput setBorderStyle:UITextBorderStyleRoundedRect];
+    
+    // SEARCH Background label
+    UIView* v = [[UIView alloc] initWithFrame:CGRectMake(20.0f, 100.0f, 280.0f, 100.0f)];
+    CAGradientLayer* lay = [CAGradientLayer layer];
+    lay.colors = [NSArray arrayWithObjects:
+                  (id)[UIColor colorWithWhite:1 alpha:1].CGColor,
+                  [UIColor colorWithRed:0.9 green:0.3 blue:0.0 alpha:0].CGColor, nil];
+    lay.frame = v.layer.bounds;
+    [v.layer addSublayer:lay];
+    lay.borderWidth = 0.5;
+    lay.borderColor = [UIColor grayColor].CGColor;
+    lay.cornerRadius = 8;
+    
+    [v addSubview:searchInput];
+    [v addSubview:searchTitle];
+    [self.view addSubview:v];
+    
+    // ABOUT button
+    aboutBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [aboutBtn setFrame:CGRectMake(280.0f, 350.0f, 19.0f, 50.0f)];
+    [aboutBtn setTitle:@"About" forState:UIControlStateNormal];
+    [aboutBtn setBackgroundImage:[UIImage imageNamed:@"info.png"] forState:UIControlStateNormal];
+    [aboutBtn addTarget:self action:@selector(goToAbout:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:aboutBtn];
+    
+    // Starts the location system
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    // Once configured, the location manager must be "started".
+    NSLog(@"startUpdatingLocation");
+    [self setLocation: locationManager.location];
+  	[locationManager startUpdatingLocation];
+	[locationManager startMonitoringSignificantLocationChanges];
+	NSLog(@"END searchCloseRistorantiView");
+    
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    [segue.destinationViewController setRistos:listOfRisto];
+}
+
+- (void)responseParsed: (NSArray*)array{
+    [self setListOfRisto:array];
+    NSLog(@"listOfRisto count = %u", [listOfRisto count]);
+    [alertView dismissWithClickedButtonIndex:0 animated:YES];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+    [self performSegueWithIdentifier:@"home2ristolist" sender:self];
+//    ListRestaurants *ristos = [[ListRestaurants alloc] initWithStyle: UITableViewStylePlain];
+//    [ristos setRistos:listOfRisto];
+//    [[self navigationController] pushViewController: ristos animated: YES];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+    NSLog(@"Location updated ");  
+    [self setLocation:newLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"Error updating the location %@", [error localizedFailureReason] );  
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
+    [super viewDidUnload];    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+
+- (void)stopRequestEvent{
+    [alertView dismissWithClickedButtonIndex:0 animated:YES];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated
